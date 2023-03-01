60853589285deeaff66193c3052c655a7485d510
rajasee <thakrejasee341@gmail.com>
2023-03-01 21:43:33 +0530

    first commit

--- oop-2/curve1.js
+++
@@ -1,4 +0,0 @@
-var canvas = document.getElementById("mycanvas");
-var context = canvas.getContext('2d');
-var c = new curve(150, 250, canvas, context);
-c.draw();
--- oop-2/curve1.ts
+++
@@ -1,5 +0,0 @@
-var canvas:HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("mycanvas");
-var context:CanvasRenderingContext2D = canvas.getContext('2d');
-
-let c:curve = new curve(150,250,canvas,context);
-c.draw();
